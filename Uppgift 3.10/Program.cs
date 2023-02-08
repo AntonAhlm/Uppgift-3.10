43b45bb61b06c9bf5a231bb56a93abf982dde461
Anton <>
2023-02-08 09:59:47 +0100

    Add project files.

---
+++ Uppgift 3.10/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Vilken plats kom du på? : ");
+            string val = Console.ReadLine(); 
+
+            switch (val)
+            {
+                case "1":
+                    Console.WriteLine("Du fick guld ");
+                    break;
+                case "2":
+                    Console.WriteLine("Du fick silver ");
+                    break;
+                case "3":
+                    Console.WriteLine("Du fick brons ");
+                    break;
+                default:
+                    Console.WriteLine("Du fick ingen medalj");
+                    break;
+
+            }
+
+
+        }
+    }
+}
